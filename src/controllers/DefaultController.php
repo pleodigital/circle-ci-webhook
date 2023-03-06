fff84856e5b5a0faa64f9ba8732ea3da65a03f71
Szymon <s.stawowy@pleodigital.com>
2023-03-06 17:38:19 +0100

    craft 4 updates

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -46,7 +46,7 @@ class DefaultController extends Controller
      *         The actions must be in 'kebab-case'
      * @access protected
      */
-    protected $allowAnonymous = [];
+    protected array|bool|int $allowAnonymous = [];
 
     // Public Methods
     // =========================================================================
